9afb6e1f5f2f84105276bbab17c7c97806130413
Ciurea Cristian <123569444+Ciureacristian@users.noreply.github.com>
2023-12-29 11:58:50 -1000

    mobile

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-keys="${e.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
@@ -9,10 +9,9 @@ function playSound(e) {
 
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
-
   this.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll(`.key`);
-keys.forEach((key) => key.addEventListener('transitioned', removeTransition));
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
